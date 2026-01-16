Stranger6667/jsonschema

fix: Evaluation paths to exclude `$ref` / `$dynamicRef` / `$recursiveRef`
Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

crates/jsonschema/src/keywords/additional_items.rs
@@ -3,9 +3,9 @@ use crate::{
     error::{no_error, ErrorIterator, ValidationError},
     keywords::{boolean::FalseValidator, CompilationResult},
     node::SchemaNode,
-    paths::{LazyLocation, Location},
+    paths::{LazyLocation, LazyRefPath, Location},
     types::{JsonType, JsonTypeSet},
-    validator::{Validate, ValidationContext},
+    validator::{capture_evaluation_path, LightweightContext, Validate, ValidationContext},
 };
 use serde_json::{Map, Value};
 
@@ -28,7 +28,7 @@ impl AdditionalItemsObjectValidator {
     }
 }
 impl Validate for AdditionalItemsObjectValidator {
-    fn is_valid(&self, instance: &Value, ctx: &mut ValidationContext) -> bool {
+    fn is_valid(&self, instance: &Value, ctx: &mut LightweightContext) -> bool {
         if let Value::Array(items) = instance {
             items
                 .iter()
@@ -43,11 +43,13 @@ impl Validate for AdditionalItemsObjectValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Array(items) = instance {
             for (idx, item) in items.iter().enumerate().skip(self.items_count) {
-                self.node.validate(item, &location.push(idx), ctx)?;
+                self.node
+                    .validate(item, &location.push(idx), ref_path, ctx)?;
             }
         }
         Ok(())
@@ -57,12 +59,16 @@ impl Validate for AdditionalItemsObjectValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Array(items) = instance {
             let mut errors = Vec::new();
             for (idx, item) in items.iter().enumerate().skip(self.items_count) {
-                errors.extend(self.node.iter_errors(item, &location.push(idx), ctx));
+                errors.extend(
+                    self.node
+                        .iter_errors(item, &location.push(idx), ref_path, ctx),
+                );
             }
             ErrorIterator::from_iterator(errors.into_iter())
         } else {
@@ -85,7 +91,7 @@ impl AdditionalItemsBooleanValidator {
     }
 }
 impl Validate for AdditionalItemsBooleanValidator {
-    fn is_valid(&self, instance: &Value, _ctx: &mut ValidationContext) -> bool {
+    fn is_valid(&self, instance: &Value, _ctx: &mut LightweightContext) -> bool {
         if let Value::Array(items) = instance {
             if items.len() > self.items_count {
                 return false;
@@ -98,12 +104,14 @@ impl Validate for AdditionalItemsBooleanValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         _ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Array(items) = instance {
             if items.len() > self.items_count {
                 return Err(ValidationError::additional_items(
                     self.location.clone(),
+                    capture_evaluation_path(&self.location, ref_path),
                     location.into(),
                     instance,
                     self.items_count,
@@ -147,15 +155,19 @@ pub(crate) fn compile<'a>(
                     Some(FalseValidator::compile(location))
                 }
             }
-            _ => Some(Err(ValidationError::multiple_type_error(
-                Location::new(),
-                ctx.location().clone(),
-                schema,
-                JsonTypeSet::empty()
-                    .insert(JsonType::Object)
-                    .insert(JsonType::Array)
-                    .insert(JsonType::Boolean),
-            ))),
+            _ => {
+                let location = ctx.location().join("additionalItems");
+                Some(Err(ValidationError::multiple_type_error(
+                    location.clone(),
+                    location,
+                    Location::new(),
+                    schema,
+                    JsonTypeSet::empty()
+                        .insert(JsonType::Object)
+                        .insert(JsonType::Array)
+                        .insert(JsonType::Boolean),
+                )))
+            }
         }
     } else {
         None

crates/jsonschema/src/keywords/dependencies.rs
@@ -3,9 +3,9 @@ use crate::{
     error::{no_error, ErrorIterator, ValidationError},
     keywords::{required, unique_items, CompilationResult},
     node::SchemaNode,
-    paths::{LazyLocation, Location},
+    paths::{LazyLocation, LazyRefPath, Location},
     types::JsonType,
-    validator::{EvaluationResult, Validate, ValidationContext},
+    validator::{EvaluationResult, LightweightContext, Validate, ValidationContext},
 };
 use serde_json::{Map, Value};
 
@@ -37,9 +37,11 @@ impl DependenciesValidator {
             }
             Ok(Box::new(DependenciesValidator { dependencies }))
         } else {
+            let location = ctx.location().join("dependencies");
             Err(ValidationError::single_type_error(
+                location.clone(),
+                location,
                 Location::new(),
-                ctx.location().clone(),
                 schema,
                 JsonType::Object,
             ))
@@ -48,10 +50,10 @@ impl DependenciesValidator {
 }
 
 impl Validate for DependenciesValidator {
-    fn is_valid(&self, instance: &Value, ctx: &mut ValidationContext) -> bool {
+    fn is_valid(&self, instance: &Value, ctx: &mut LightweightContext) -> bool {
         if let Value::Object(item) = instance {
             for (property, node) in &self.dependencies {
-                if item.contains_key(property) && !node.is_valid(instance, ctx) {
+                if item.contains_key(property) && !node.is_valid(instance, ctx.lightweight()) {
                     return false;
                 }
             }
@@ -65,12 +67,13 @@ impl Validate for DependenciesValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Object(item) = instance {
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    dependency.validate(instance, location, ctx)?;
+                    dependency.validate(instance, location, ref_path, ctx)?;
                 }
             }
         }
@@ -81,13 +84,14 @@ impl Validate for DependenciesValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Object(item) = instance {
             let mut errors = Vec::new();
             for (property, node) in &self.dependencies {
                 if item.contains_key(property) {
-                    errors.extend(node.iter_errors(instance, location, ctx));
+                    errors.extend(node.iter_errors(instance, location, ref_path, ctx));
                 }
             }
             ErrorIterator::from_iterator(errors.into_iter())
@@ -100,13 +104,14 @@ impl Validate for DependenciesValidator {
         &self,
         instance: &Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> EvaluationResult {
         if let Value::Object(item) = instance {
             let mut children = Vec::new();
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    children.push(dependency.evaluate_instance(instance, location, ctx));
+                    children.push(dependency.evaluate_instance(instance, location, ref_path, ctx));
                 }
             }
             EvaluationResult::from_children(children)
@@ -130,9 +135,11 @@ impl DependentRequiredValidator {
                 let ictx = kctx.new_at_location(key.as_str());
                 if let Value::Array(dependency_array) = subschema {
                     if !unique_items::is_unique(dependency_array) {
+                        let location = ictx.location().clone();
                         return Err(ValidationError::unique_items(
+                            location.clone(),
+                            location,
                             Location::new(),
-                            ictx.location().clone(),
                             subschema,
                         ));
                     }
@@ -145,9 +152,11 @@ impl DependentRequiredValidator {
                         ];
                     dependencies.push((key.clone(), SchemaNode::from_array(&kctx, validators)));
                 } else {
+                    let location = ictx.location().clone();
                     return Err(ValidationError::single_type_error(
+                        location.clone(),
+                        location,
                         Location::new(),
-                        ictx.location().clone(),
                         subschema,
                         JsonType::Array,
                     ));
@@ -155,9 +164,11 @@ impl DependentRequiredValidator {
             }
             Ok(Box::new(DependentRequiredValidator { dependencies }))
         } else {
+            let location = ctx.location().join("dependentRequired");
             Err(ValidationError::single_type_error(
+                location.clone(),
+                location,
                 Location::new(),
-                ctx.location().clone(),
                 schema,
                 JsonType::Object,
             ))
@@ -165,10 +176,10 @@ impl DependentRequiredValidator {
     }
 }
 impl Validate for DependentRequiredValidator {
-    fn is_valid(&self, instance: &Value, ctx: &mut ValidationContext) -> bool {
+    fn is_valid(&self, instance: &Value, ctx: &mut LightweightContext) -> bool {
         if let Value::Object(item) = instance {
             for (property, node) in &self.dependencies {
-                if item.contains_key(property) && !node.is_valid(instance, ctx) {
+                if item.contains_key(property) && !node.is_valid(instance, ctx.lightweight()) {
                     return false;
                 }
             }
@@ -182,12 +193,13 @@ impl Validate for DependentRequiredValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Object(item) = instance {
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    dependency.validate(instance, location, ctx)?;
+                    dependency.validate(instance, location, ref_path, ctx)?;
                 }
             }
         }
@@ -198,13 +210,14 @@ impl Validate for DependentRequiredValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Object(item) = instance {
             let mut errors = Vec::new();
             for (property, node) in &self.dependencies {
                 if item.contains_key(property) {
-                    errors.extend(node.iter_errors(instance, location, ctx));
+                    errors.extend(node.iter_errors(instance, location, ref_path, ctx));
                 }
             }
             ErrorIterator::from_iterator(errors.into_iter())
@@ -217,13 +230,14 @@ impl Validate for DependentRequiredValidator {
         &self,
         instance: &Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> EvaluationResult {
         if let Value::Object(item) = instance {
             let mut children = Vec::new();
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    children.push(dependency.evaluate_instance(instance, location, ctx));
+                    children.push(dependency.evaluate_instance(instance, location, ref_path, ctx));
                 }
             }
             EvaluationResult::from_children(children)
@@ -249,9 +263,11 @@ impl DependentSchemasValidator {
             }
             Ok(Box::new(DependentSchemasValidator { dependencies }))
         } else {
+            let location = ctx.location().join("dependentSchemas");
             Err(ValidationError::single_type_error(
+                location.clone(),
+                location,
                 Location::new(),
-                ctx.location().clone(),
                 schema,
                 JsonType::Object,
             ))
@@ -259,10 +275,10 @@ impl DependentSchemasValidator {
     }
 }
 impl Validate for DependentSchemasValidator {
-    fn is_valid(&self, instance: &Value, ctx: &mut ValidationContext) -> bool {
+    fn is_valid(&self, instance: &Value, ctx: &mut LightweightContext) -> bool {
         if let Value::Object(item) = instance {
             for (property, node) in &self.dependencies {
-                if item.contains_key(property) && !node.is_valid(instance, ctx) {
+                if item.contains_key(property) && !node.is_valid(instance, ctx.lightweight()) {
                     return false;
                 }
             }
@@ -276,12 +292,13 @@ impl Validate for DependentSchemasValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Object(item) = instance {
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    dependency.validate(instance, location, ctx)?;
+                    dependency.validate(instance, location, ref_path, ctx)?;
                 }
             }
         }
@@ -292,13 +309,14 @@ impl Validate for DependentSchemasValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Object(item) = instance {
             let mut errors = Vec::new();
             for (property, node) in &self.dependencies {
                 if item.contains_key(property) {
-                    errors.extend(node.iter_errors(instance, location, ctx));
+                    errors.extend(node.iter_errors(instance, location, ref_path, ctx));
                 }
             }
             ErrorIterator::from_iterator(errors.into_iter())
@@ -311,13 +329,14 @@ impl Validate for DependentSchemasValidator {
         &self,
         instance: &Value,
         location: &LazyLocation,
+        ref_path: &LazyRefPath,
         ctx: &mut ValidationContext,
     ) -> EvaluationResult {
         if let Value::Object(item) = instance {
             let mut children = Vec::new();
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    children.push(dependency.evaluate_instance(instance, location, ctx));
+                    children.push(dependency.evaluate_instance(instance, location, ref_path, ctx));
                 }
             }
             EvaluationResult::from_children(children)

kod-kristoff/jsonschema-rs

crates/jsonschema/src/keywords/additional_items.rs
@@ -3,7 +3,7 @@ use crate::{
     error::{no_error, ErrorIterator, ValidationError},
     keywords::{boolean::FalseValidator, CompilationResult},
     node::SchemaNode,
-    paths::{LazyLocation, Location},
+    paths::{LazyLocation, Location, RefTracker},
     types::{JsonType, JsonTypeSet},
     validator::{Validate, ValidationContext},
 };
@@ -43,11 +43,13 @@ impl Validate for AdditionalItemsObjectValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Array(items) = instance {
             for (idx, item) in items.iter().enumerate().skip(self.items_count) {
-                self.node.validate(item, &location.push(idx), ctx)?;
+                self.node
+                    .validate(item, &location.push(idx), tracker, ctx)?;
             }
         }
         Ok(())
@@ -57,12 +59,16 @@ impl Validate for AdditionalItemsObjectValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Array(items) = instance {
             let mut errors = Vec::new();
             for (idx, item) in items.iter().enumerate().skip(self.items_count) {
-                errors.extend(self.node.iter_errors(item, &location.push(idx), ctx));
+                errors.extend(
+                    self.node
+                        .iter_errors(item, &location.push(idx), tracker, ctx),
+                );
             }
             ErrorIterator::from_iterator(errors.into_iter())
         } else {
@@ -98,12 +104,14 @@ impl Validate for AdditionalItemsBooleanValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         _ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Array(items) = instance {
             if items.len() > self.items_count {
                 return Err(ValidationError::additional_items(
                     self.location.clone(),
+                    crate::paths::capture_evaluation_path(tracker, &self.location),
                     location.into(),
                     instance,
                     self.items_count,
@@ -147,15 +155,19 @@ pub(crate) fn compile<'a>(
                     Some(FalseValidator::compile(location))
                 }
             }
-            _ => Some(Err(ValidationError::multiple_type_error(
-                Location::new(),
-                ctx.location().clone(),
-                schema,
-                JsonTypeSet::empty()
-                    .insert(JsonType::Object)
-                    .insert(JsonType::Array)
-                    .insert(JsonType::Boolean),
-            ))),
+            _ => {
+                let location = ctx.location().join("additionalItems");
+                Some(Err(ValidationError::multiple_type_error(
+                    location.clone(),
+                    location,
+                    Location::new(),
+                    schema,
+                    JsonTypeSet::empty()
+                        .insert(JsonType::Object)
+                        .insert(JsonType::Array)
+                        .insert(JsonType::Boolean),
+                )))
+            }
         }
     } else {
         None

crates/jsonschema/src/keywords/dependencies.rs
@@ -3,7 +3,7 @@ use crate::{
     error::{no_error, ErrorIterator, ValidationError},
     keywords::{required, unique_items, CompilationResult},
     node::SchemaNode,
-    paths::{LazyLocation, Location},
+    paths::{LazyLocation, Location, RefTracker},
     types::JsonType,
     validator::{EvaluationResult, Validate, ValidationContext},
 };
@@ -37,9 +37,11 @@ impl DependenciesValidator {
             }
             Ok(Box::new(DependenciesValidator { dependencies }))
         } else {
+            let location = ctx.location().join("dependencies");
             Err(ValidationError::single_type_error(
+                location.clone(),
+                location,
                 Location::new(),
-                ctx.location().clone(),
                 schema,
                 JsonType::Object,
             ))
@@ -65,12 +67,13 @@ impl Validate for DependenciesValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Object(item) = instance {
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    dependency.validate(instance, location, ctx)?;
+                    dependency.validate(instance, location, tracker, ctx)?;
                 }
             }
         }
@@ -81,13 +84,14 @@ impl Validate for DependenciesValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Object(item) = instance {
             let mut errors = Vec::new();
             for (property, node) in &self.dependencies {
                 if item.contains_key(property) {
-                    errors.extend(node.iter_errors(instance, location, ctx));
+                    errors.extend(node.iter_errors(instance, location, tracker, ctx));
                 }
             }
             ErrorIterator::from_iterator(errors.into_iter())
@@ -100,13 +104,14 @@ impl Validate for DependenciesValidator {
         &self,
         instance: &Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> EvaluationResult {
         if let Value::Object(item) = instance {
             let mut children = Vec::new();
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    children.push(dependency.evaluate_instance(instance, location, ctx));
+                    children.push(dependency.evaluate_instance(instance, location, tracker, ctx));
                 }
             }
             EvaluationResult::from_children(children)
@@ -130,9 +135,11 @@ impl DependentRequiredValidator {
                 let ictx = kctx.new_at_location(key.as_str());
                 if let Value::Array(dependency_array) = subschema {
                     if !unique_items::is_unique(dependency_array) {
+                        let location = ictx.location().clone();
                         return Err(ValidationError::unique_items(
+                            location.clone(),
+                            location,
                             Location::new(),
-                            ictx.location().clone(),
                             subschema,
                         ));
                     }
@@ -145,9 +152,11 @@ impl DependentRequiredValidator {
                         ];
                     dependencies.push((key.clone(), SchemaNode::from_array(&kctx, validators)));
                 } else {
+                    let location = ictx.location().clone();
                     return Err(ValidationError::single_type_error(
+                        location.clone(),
+                        location,
                         Location::new(),
-                        ictx.location().clone(),
                         subschema,
                         JsonType::Array,
                     ));
@@ -155,9 +164,11 @@ impl DependentRequiredValidator {
             }
             Ok(Box::new(DependentRequiredValidator { dependencies }))
         } else {
+            let location = ctx.location().join("dependentRequired");
             Err(ValidationError::single_type_error(
+                location.clone(),
+                location,
                 Location::new(),
-                ctx.location().clone(),
                 schema,
                 JsonType::Object,
             ))
@@ -182,12 +193,13 @@ impl Validate for DependentRequiredValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Object(item) = instance {
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    dependency.validate(instance, location, ctx)?;
+                    dependency.validate(instance, location, tracker, ctx)?;
                 }
             }
         }
@@ -198,13 +210,14 @@ impl Validate for DependentRequiredValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Object(item) = instance {
             let mut errors = Vec::new();
             for (property, node) in &self.dependencies {
                 if item.contains_key(property) {
-                    errors.extend(node.iter_errors(instance, location, ctx));
+                    errors.extend(node.iter_errors(instance, location, tracker, ctx));
                 }
             }
             ErrorIterator::from_iterator(errors.into_iter())
@@ -217,13 +230,14 @@ impl Validate for DependentRequiredValidator {
         &self,
         instance: &Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> EvaluationResult {
         if let Value::Object(item) = instance {
             let mut children = Vec::new();
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    children.push(dependency.evaluate_instance(instance, location, ctx));
+                    children.push(dependency.evaluate_instance(instance, location, tracker, ctx));
                 }
             }
             EvaluationResult::from_children(children)
@@ -249,9 +263,11 @@ impl DependentSchemasValidator {
             }
             Ok(Box::new(DependentSchemasValidator { dependencies }))
         } else {
+            let location = ctx.location().join("dependentSchemas");
             Err(ValidationError::single_type_error(
+                location.clone(),
+                location,
                 Location::new(),
-                ctx.location().clone(),
                 schema,
                 JsonType::Object,
             ))
@@ -276,12 +292,13 @@ impl Validate for DependentSchemasValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> Result<(), ValidationError<'i>> {
         if let Value::Object(item) = instance {
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    dependency.validate(instance, location, ctx)?;
+                    dependency.validate(instance, location, tracker, ctx)?;
                 }
             }
         }
@@ -292,13 +309,14 @@ impl Validate for DependentSchemasValidator {
         &self,
         instance: &'i Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> ErrorIterator<'i> {
         if let Value::Object(item) = instance {
             let mut errors = Vec::new();
             for (property, node) in &self.dependencies {
                 if item.contains_key(property) {
-                    errors.extend(node.iter_errors(instance, location, ctx));
+                    errors.extend(node.iter_errors(instance, location, tracker, ctx));
                 }
             }
             ErrorIterator::from_iterator(errors.into_iter())
@@ -311,13 +329,14 @@ impl Validate for DependentSchemasValidator {
         &self,
         instance: &Value,
         location: &LazyLocation,
+        tracker: Option<&RefTracker>,
         ctx: &mut ValidationContext,
     ) -> EvaluationResult {
         if let Value::Object(item) = instance {
             let mut children = Vec::new();
             for (property, dependency) in &self.dependencies {
                 if item.contains_key(property) {
-                    children.push(dependency.evaluate_instance(instance, location, ctx));
+                    children.push(dependency.evaluate_instance(instance, location, tracker, ctx));
                 }
             }
             EvaluationResult::from_children(children)